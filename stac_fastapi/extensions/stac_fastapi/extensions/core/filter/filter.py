3dd1e665891df129d9bd4c08ca9d9071e5a7f54a
tjellicoe <thomas.jellicoe@telespazio.com>
2025-01-30 11:36:49 +0000

    reorder queryables endpoints

--- stac_fastapi/extensions/stac_fastapi/extensions/core/filter/filter.py
+++ stac_fastapi/extensions/stac_fastapi/extensions/core/filter/filter.py
@@ -94,8 +94,8 @@ class FilterExtension(ApiExtension):
         """
         self.router.prefix = app.state.router_prefix
         self.router.add_api_route(
-            name="Queryables",
-            path="/queryables",
+            name="Collection Queryables",
+            path="/catalogs/{cat_path:path}/collections/{collection_id}/queryables",
             methods=["GET"],
             responses={
                 200: {
@@ -106,9 +106,8 @@ class FilterExtension(ApiExtension):
                 },
             },
             response_class=self.response_class,
-            endpoint=create_async_endpoint(self.client.get_queryables, EmptyRequest),
+            endpoint=create_async_endpoint(self.client.get_queryables, CollectionUri),
         )
-        self.router.prefix = app.state.router_prefix
         self.router.add_api_route(
             name="Catalog Queryables",
             path="/catalogs/{cat_path:path}/queryables",
@@ -125,8 +124,8 @@ class FilterExtension(ApiExtension):
             endpoint=create_async_endpoint(self.client.get_queryables, CatalogUri),
         )
         self.router.add_api_route(
-            name="Collection Queryables",
-            path="/catalogs/{cat_path:path}/collections/{collection_id}/queryables",
+            name="Queryables",
+            path="/queryables",
             methods=["GET"],
             responses={
                 200: {
@@ -137,6 +136,6 @@ class FilterExtension(ApiExtension):
                 },
             },
             response_class=self.response_class,
-            endpoint=create_async_endpoint(self.client.get_queryables, CollectionUri),
+            endpoint=create_async_endpoint(self.client.get_queryables, EmptyRequest),
         )
         app.include_router(self.router, tags=["Filter Extension"])
